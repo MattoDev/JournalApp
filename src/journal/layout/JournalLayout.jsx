b83a7b1504b5a713e25ba2ffca9520efc522817f
MattoDev <mateofonseca.profesional@gmail.com>
2024-01-15 18:44:06 -0500

    JournalLayout and JournalPage

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+
+const drawerWidth = 240;
+
+// Cualquier otra pantalla o pagina va a poder utilizar el mismo diseño y heredar todo lo de journalLayout
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/* Navbar  drawerWidth*/}
+
+      {/* Sidebar  drawerWidth*/}
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* Toolbar */}
+
+        {children}
+      </Box>
+    </Box>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,20 @@
 import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+        las industrias desde el año 1500, cuando un impresor (N. del T. persona
+        que se dedica a la imprenta) desconocido usó una galería de textos y los
+        mezcló de tal manera que logró hacer un libro de textos especimen. No
+        sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno
+        en documentos electrónicos, quedando esencialmente igual al original.
+        Fue popularizado en los 60s con la creación de las hojas "Letraset", la
+      </Typography>
+    </JournalLayout>
   );
 };
