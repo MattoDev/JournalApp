da484d2c7b10ebccaa7b717217b3b4bb57ea2e95
MattoDev <mateofonseca.profesional@gmail.com>
2024-01-15 21:09:50 -0500

    Create SideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import { Navbar } from "../components";
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -11,9 +11,11 @@ export const JournalLayout = ({ children }) => {
       <Navbar drawerWidth={drawerWidth} />
 
       {/* Sidebar  drawerWidth*/}
+      <SideBar drawerWidth={drawerWidth} />
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
+        <Toolbar />
 
         {children}
       </Box>
