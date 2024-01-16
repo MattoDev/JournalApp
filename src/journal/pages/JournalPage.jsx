567efba5c5347908e7d737868aae594117130c59
MattoDev <mateofonseca.profesional@gmail.com>
2024-01-15 21:39:13 -0500

    Create NothingSlectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Lorem Ipsum es simplemente el texto de relleno de las imprentas y
         archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
         las industrias desde el año 1500, cuando un impresor (N. del T. persona
@@ -14,7 +15,9 @@ export const JournalPage = () => {
         sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno
         en documentos electrónicos, quedando esencialmente igual al original.
         Fue popularizado en los 60s con la creación de las hojas "Letraset", la
-      </Typography>
+      </Typography> */}
+
+      <NothingSelectedView />
     </JournalLayout>
   );
 };
