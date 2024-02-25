bea65efe53598c33a890b909d5bb204d171f4c53
MattoDev <mateofonseca.profesional@gmail.com>
2024-02-24 21:45:40 -0500

    Checking authentication

---
+++ src/ui/components/CheckingAuth.jsx
@@ -0,0 +1,20 @@
+import { CircularProgress, Grid } from "@mui/material";
+
+export const CheckingAuth = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      {/* Esta parte es para poner la caja en medio */}
+      {/* xs tamaño de las pantallas investigar */}
+      <Grid item direction="row" justifyContent="center">
+        <CircularProgress color="warning" />
+      </Grid>
+    </Grid>
+  );
+};
