74732c01ab1812b62b230b3ce1cb2049acac42ec
MattoDev <mateofonseca.profesional@gmail.com>
2024-02-25 17:25:07 -0500

    JournalSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth";
+import { journalSlice } from "./journal";
 
 //Esta es mi fuente unica de la verdad
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    journal: journalSlice.reducer,
   },
 });
