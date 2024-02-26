2c4d399ec27b264c25fff76642627f2ecec9b93d
MattoDev <mateofonseca.profesional@gmail.com>
2024-02-26 16:41:49 -0500

    activate note

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -7,15 +7,22 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal";
 
-export const SideBarItem = ({ title = "", body, id }) => {
+export const SideBarItem = ({ id, title = "", body, date, imageUrls = [] }) => {
+  const dispatch = useDispatch();
   const newTilte = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
   }, [title]);
 
+  const onSetActiveNote = () => {
+    dispatch(setActiveNote({ id, title, body, date, imageUrls }));
+  };
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onSetActiveNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
