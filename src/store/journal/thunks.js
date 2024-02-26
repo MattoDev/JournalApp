585e11325823ee298a4b50fe37e26ca769c9d36a
MattoDev <mateofonseca.profesional@gmail.com>
2024-02-26 11:55:12 -0500

    create newNote

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,12 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+
 // La nomencaltura start me quiere decir cuando inicia el proceso
-export const startNewNote = async () => {
-  return async (dispatch) => {
+export const startNewNote = () => {
+  //get state es el segundo argumento dentro del thunk el cual es una funcion
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    console.log("startNewNote");
     //uid
 
     const newNote = {
@@ -8,6 +14,12 @@ export const startNewNote = async () => {
       body: "",
       date: new Date().getTime(),
     };
+
+    //Crear la referencia del documento donde voy a insertar
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+
+    const setDocResp = await setDoc(newDoc, newNote);
+    console.log({ newDoc, setDoc });
 
     //dispatch
     //dispatch(newNote)
